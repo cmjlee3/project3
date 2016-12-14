0c5bea052f1e0d38a3daea1ec3d14a1df6014a4e
Cecil Lee <cmjlee3@gmail.com>
2016-12-14 15:56:38 -0500

    heroku

--- routes/map.js
+++ routes/map.js
@@ -3,19 +3,19 @@ const { authenticate } = require('../lib/auth');
 const { getLocations, getListing, saveMapLocation, getAllMarkers, deleteMarker } = require('../models/locations');
 const { searchListings } = require('../models/search');
 
-// router.get('/allmarkers', getAllMarkers, (req,res) => { // ADD getAllMarkers after creating it in model/locations
+// router.get('/test', getAllMarkers, (req,res) => { // ADD getAllMarkers after creating it in model/locations
 //   console.log('HERE');
-//   res.render('map/index', {
-//     allMapMarkers: res.allMarkers || [],
-//   })
-//   // res.json({status: 'ok'})
+//   // res.render('map/index', {
+//   //   allMapMarkers: res.allMarkers || [],
+//   // })
+//   res.json({markers: res.allMarkers || []});
 // });
 
 router.get('/', authenticate, getLocations, getAllMarkers, (req,res) => { // ADD getAllMarkers after creating it in model/locations
   // console.log('HERE');
   res.render('map/index', {
    mapMarkers: res.allListings || [],
-   allMapMarkers: res.allMarkers || [],
+   allMapMarkers: res.allMarkers || []
   })
 });
 
